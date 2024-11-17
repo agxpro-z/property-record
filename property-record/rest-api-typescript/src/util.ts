0e35cb144289920c56f28c0ed20e06852faf6ad5
agxpro <herrscherofdarkness@gmail.com>
2024-11-17 14:40:32 +0530

    RestApi: Log user to console everytime request is made.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- property-record/rest-api-typescript/src/util.ts
+++ property-record/rest-api-typescript/src/util.ts
@@ -4,10 +4,9 @@ import { createGateway, getContracts, getNetwork } from './fabric';
 import { Contract } from 'fabric-network';
 
 export const getUserContract = async (app: Application, userId: string): Promise<Contract> => {
-  console.log(userId);
-
   if (['Org1MSP', 'Org2MSP', 'Org3MSP', 'Org4MSP', 'Org5MSP'].includes(userId)) {
     const contract = app.locals[userId]?.assetContract;
+    console.log(`Admin user: ${userId}`);
     return contract;
   } else {
     const user = await User.findOne({ email: userId });
@@ -16,6 +15,7 @@ export const getUserContract = async (app: Application, userId: string): Promise
       userId,
       app.locals.wallet
     );
+    console.log(`User: ${userId}, Org: ${user?.orgId}`);
     const network = await getNetwork(gateway);
     const contract = await getContracts(network);
     return contract.assetContract;
